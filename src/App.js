8fb4eb1f6404246e9f2446e8d8bdcd9c1f0730f5
Axel Aquino <aaquinojimenez@ufl.edu>
2023-01-27 18:54:45 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import logo from './gator.gif';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Hello World!This is our team project. <
-        /p>	 < /
-        header > <
-        /div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
+      </header>
+    </div>
+  );
 }
 
 export default App;
